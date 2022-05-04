d45110e3e872a2fa85639d06ab5784e955aef6a7
t-ohtsuka89 <102138393+t-ohtsuka89@users.noreply.github.com>
2022-05-04 17:57:36 +0000

    solve abc087-b

---
+++ abc087/b/main.py
@@ -0,0 +1,12 @@
+A = int(input())
+B = int(input())
+C = int(input())
+X = int(input())
+
+ans = 0
+for i in range(A + 1):
+    for j in range(B + 1):
+        for k in range(C + 1):
+            if i * 500 + j * 100 + k * 50 == X:
+                ans += 1
+print(ans)
